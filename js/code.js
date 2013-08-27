a7fe3957c22f5191e70be29d495f2fd41726f184
Saulo dos Santos Firmino <saulofirmino@ymail.com>
2013-08-27 11:01:32 -0300

    contador quase pronto
    agora falta acertar uma coisa no contador e acertar layout....
    sobre layout, infelizmente terei que mexer nele com js ao inves de
    somente css...

--- js/code.js
+++ js/code.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 		$('#slider-code').tinycarousel({controls: true});	
-		
+		$('#counter').countdown({until: new Date("September 10, 2013 10:00:00"),format: 'DHMS'});
+		//$('#counter').countdown({until: new Date(),format: 'DHMS'});
 		
 });
