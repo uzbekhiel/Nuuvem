e24f0458f9afe00c6c610bc505da744795dbf622
Saulo dos Santos Firmino <saulofirmino@ymail.com>
2013-08-29 18:02:17 -0300

    acertando o layout 2

--- js/code.js
+++ js/code.js
@@ -1,19 +1,17 @@
 $(document).ready(function(){
+		$('#slider-code .overview li').append("<img src=\"img/carousel-bg.png\" class=\"carousel-bg\" /> ");
+		
+		$('.search').hide();
+
 		$('#slider-code').tinycarousel({controls: true});	
 		$('#counter').countdown({until: new Date("September 10, 2013 10:00:00"),format: 'DHMS'});
 		
-		$('.border').mouseover(function() {
-		  $(this).css({ border: '2px solid #EAAB04' });
-		  $(this).append("<img src=\"img/search-img.png\" class=\"search\" /> ");
+		$('.overview a').mouseover(function() {
+		  $(this).children('div').css({ border: '2px solid #EAAB04' });
+		  $(this).children('div').children('.search').show();
 		}).mouseout(function() {
-		  $(this).css({ border: '2px solid white' });
-		  $('.search').remove();
+		  $(this).children('div').css({ border: '2px solid white' });
+		  $('.search').hide();
 		});
-		
-		$('.search').mouseover(function() {
-		  $(this).css({ border: '2px solid #EAAB04' });
-		  $(this).append("<img src=\"img/search-img.png\" class=\"search\" /> ");
-		});
-		
 		
 });
